7ca64f8b22289dfa12ea17de8c118d51740210f4
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-24 08:21:44 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <div style={{border: '2px solid darkred'}}>Something</div>
+    </div>
+  );
+}
+
+export default App;
