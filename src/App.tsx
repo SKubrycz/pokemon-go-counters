bc077a33bb931bee5562a4e53aee16cfce22b945
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-24 08:37:29 +0100

    Update App.tsx
    added primary containers

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import './App.scss';
 
+import Bug from './assets/Bug.svg';
+
 function App() {
   return (
     <div className='app'>
-      <div style={{border: '2px solid darkred'}}>Something</div>
+      <div className='title'>Pokemon Type Counters</div>
+      <div className='types-container'>
+        <div className='types-chosen'>Chosen</div>
+        <div className='types-all'>
+          <img className='types-img' src={Bug} alt='bug'></img>
+        </div>
+      </div>
     </div>
   );
 }
