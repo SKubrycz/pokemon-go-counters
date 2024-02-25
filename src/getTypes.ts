6f4407e82b58be1bcd5c669b6bedd20ab150382f
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-25 14:22:12 +0100

    Update getTypes.ts
    added two arrays for Type objects

--- src/getTypes.ts
+++ src/getTypes.ts
@@ -6,39 +6,44 @@ import {
     Steel, Water,
   } from './image-import';
 
-export default function getTypes() {
+
+export class Type {
+    readonly src: string;
+    readonly title: string;
+    readonly alt: string;
+    readonly weakAgainst: string[];
+    readonly strongAgainst: string[];
+
+    constructor(src: string, title: string, alt: string, weakAgainst: string[], strongAgainst: string[]) {
+    this.src = src;
+    this.title = title;
+    this.alt = alt;
+    this.weakAgainst = weakAgainst;
+    this.strongAgainst = strongAgainst;
+    }
+}
+
+export function getTypes() {
     let typesArray: Type[] = [];
 
-    class Type {
-        readonly src: string;
-        readonly title: string;
-        readonly alt: string;
-      
-        constructor(src: string, title: string, alt: string) {
-          this.src = src;
-          this.title = title;
-          this.alt = alt;
-        }
-    }
-
-    const BugType = new Type(Bug, 'Bug', 'bug');
-    const DarkType = new Type(Dark, 'Dark', 'dark');
-    const DragonType = new Type(Dragon, 'Dragon', 'dragon');
-    const ElectricType = new Type(Electric, 'Electric', 'electric');
-    const FairyType = new Type(Fairy, 'Fairy', 'fairy');
-    const FightingType = new Type(Fighting, 'Fighting', 'fighting');
-    const FireType = new Type(Fire, 'Fire', 'fire');
-    const FlyingType = new Type(Flying, 'Flying', 'flying');
-    const GhostType = new Type(Ghost, 'Ghost', 'ghost');
-    const GrassType = new Type(Grass, 'Grass', 'grass');
-    const GroundType = new Type(Ground, 'Ground', 'ground');
-    const IceType = new Type(Ice, 'Ice', 'ice');
-    const NormalType = new Type(Normal, 'Normal', 'normal');
-    const PoisonType = new Type(Poison, 'Poison', 'poison');
-    const PsychicType = new Type(Psychic, 'Psychic', 'psychic');
-    const RockType = new Type(Rock, 'Rock', 'rock');
-    const SteelType = new Type(Steel, 'Steel', 'steel');
-    const WaterType = new Type(Water, 'Water', 'water');
+    const BugType = new Type(Bug, 'Bug', 'bug', ['Fire', 'Flying', 'Rock'], ['Dark', 'Grass', 'Psychic']);
+    const DarkType = new Type(Dark, 'Dark', 'dark', ['Bug', 'Fairy', 'Fighting'], ['Ghost', 'Psychic']);
+    const DragonType = new Type(Dragon, 'Dragon', 'dragon', ['Dragon', 'Fairy', 'Fighting'], ['Dragon']);
+    const ElectricType = new Type(Electric, 'Electric', 'electric', ['Ground'], ['Flying', 'Water']);
+    const FairyType = new Type(Fairy, 'Fairy', 'fairy', ['Poison', 'Steel'], ['Dark', 'Dragon', 'Fighting']);
+    const FightingType = new Type(Fighting, 'Fighting', 'fighting', ['Fairy', 'Flying', 'Psychic'], ['Dark', 'Ice', 'Normal', 'Rock', 'Steel']);
+    const FireType = new Type(Fire, 'Fire', 'fire', ['Ground', 'Rock', 'Water'], ['Bug', 'Grass', 'Ice', 'Steel']);
+    const FlyingType = new Type(Flying, 'Flying', 'flying', ['Electric', 'Ice', 'Rock'], ['Bug', 'Grass', 'Fighting']);
+    const GhostType = new Type(Ghost, 'Ghost', 'ghost', ['Dark', 'Ghost'], ['Ghost', 'Fighting']);
+    const GrassType = new Type(Grass, 'Grass', 'grass', ['Bug', 'Fire', 'Flying', 'Ice', 'Poison'], ['Ground', 'Rock', 'Water']);
+    const GroundType = new Type(Ground, 'Ground', 'ground', ['Grass', 'Ice', 'Water'], ['Electric', 'Fire', 'Poison', 'Rock', 'Steel']);
+    const IceType = new Type(Ice, 'Ice', 'ice', ['Fighting', 'Fire', 'Rock', 'Steel'], ['Dragon', 'Flying', 'Grass', 'Ground']);
+    const NormalType = new Type(Normal, 'Normal', 'normal', ['Fighting'], []);
+    const PoisonType = new Type(Poison, 'Poison', 'poison', ['Ground', 'Psychic'], ['Fairy', 'Grass']);
+    const PsychicType = new Type(Psychic, 'Psychic', 'psychic', ['Bug', 'Dark', 'Ghost'], ['Fighting', 'Poison']);
+    const RockType = new Type(Rock, 'Rock', 'rock', ['Fighting', 'Grass', 'Ground', 'Steel', 'Water'], ['Bug', 'Fire', 'Flying', 'Ice']);
+    const SteelType = new Type(Steel, 'Steel', 'steel', ['Fighting', 'Fire', 'Ground'], ['Fairy', 'Ice', 'Rock']);
+    const WaterType = new Type(Water, 'Water', 'water', ['Electric', 'Grass'], ['Fire', 'Ground', 'Rock']);
 
     typesArray = [
         BugType, DarkType, DragonType, ElectricType, 
