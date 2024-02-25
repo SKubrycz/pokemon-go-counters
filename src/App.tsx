d52c0082deddfc7840387375a2e3aa92b8cf6a3c
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-25 14:28:32 +0100

    Update App.tsx
    added types reset

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,36 @@ import {
   Reset,
 } from './image-import';
 
-import getTypes from './getTypes';
+import { Type, getTypes } from './getTypes';
 
 
 function App() {
-  const chosenRef = useRef<HTMLImageElement | null>(null);
+  //const chosenRef = useRef<HTMLImageElement | null>(null);
 
-  let typesArray: any[] = getTypes();
+  const [selectedImage, setSelectedImage] = useState<Type | null>(null);
 
-  console.log(typesArray);
+  const typesArray: Type[] = getTypes();
   
-  const chooseType = () => {
+  const chooseType = (type: Type) => {
+    setSelectedImage(type);
+    console.log(`%c Weak against: ${type.weakAgainst}`, 'color: rgb(220, 50, 50)');
+    console.log(`%c Strong against: ${type.strongAgainst}`, 'color: lightgreen');
+  }
 
+  const resetTypes = () => {
+    setSelectedImage(null);
   }
 
   return (
     <div className='app'>
       <div className='title'>Pokemon Type Counters</div>
       <div className='types-container'>
-        <div className='types-chosen' ref={chosenRef}>
+        <div className='types-chosen'>
           <div className='types-chosen-absolute'>
+            {selectedImage && <img className='types-img' src={selectedImage.src} title={selectedImage.title} alt={selectedImage.alt} />}
           </div>
           <div className='types-reset-container'>
-            <img className='types-reset' src={Reset} title='Reset' alt='reset'></img>
+            <img className='types-reset' src={Reset} onClick={resetTypes} title='Reset' alt='reset'></img>
           </div>
         </div>
         <div className='types-all'>
@@ -37,7 +44,7 @@ function App() {
               key={index}
               className='types-img'
               src={type.src}
-              onClick={chooseType}
+              onClick={() => chooseType(type)}
               title={type.title}
               alt={type.alt}
             />
