bd220c65f9f706db3bad17b59478c91cde50331a
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-03-01 11:51:57 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -224,16 +224,44 @@ function App() {
               </div>
             </div>
             <div className='types-all'>
-            {typesArray.map((type, index) => (
+            {typesArray.map((type, index) => {
+              let condition: boolean = type.title === selectedImage?.[0]?.title || type.title === selectedImage?.[1]?.title;
+
+              let colorAdjust: string;
+
+              switch(type.color) {
+                case '#414141': 
+                  colorAdjust = '#969696';
+                  break;
+                case '#828282':
+                  colorAdjust = '#C2C2C2';
+                  break;
+                case '#436E81': 
+                  colorAdjust = '#5E9AB5';
+                  break;
+                default:
+                  colorAdjust = type.color;
+              }
+
+              const style = {
+                filter: (condition) ? `drop-shadow(0px 0px 20px ${colorAdjust})` : '',
+                boxShadow: (condition) ? `0px 0px 0px 2px ${type.color}` : 'none',
+                borderRadius: (condition) ? `100%` : '0',
+              }
+
+              return (
                 <img
                   key={index}
-                  className={`types-img ${(type.title === selectedImage?.[0]?.title || type.title === selectedImage?.[1]?.title) ? 'types-all-highlight' : ''}`}
+                  className={`types-img`}
                   src={type.src}
                   onClick={() => chooseType(type)}
                   title={type.title}
                   alt={type.alt}
+                  style={style}
                 />
-              ))}
+              ); 
+              }
+              )}
             </div>
           </div>
           <div className='strong-container'>
